4dec911325e40f852d6b8b5925cd4d022f49fbf4
andrepalhares <andrepalhares@hotmail.com>
2021-03-18 18:25:54 -0300

    inicializando classe de testes

---
+++ tests/colegiado/test/ProfessorTeste.java
@@ -0,0 +1,14 @@
+package colegiado.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProfessorTeste {
+
+	@Test
+	public void test() {
+		assertEquals(1,1);
+	}
+
+}
